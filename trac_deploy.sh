971c708d7c00fd22dd30695a575a808655229bf2
Kurt R. Prudhomme <krprudhomme@gmail.com>
2013-07-05 20:03:11 -0500

    Trying to make the script better.
    Trying to make the scrip accomidate a sinlge project site and a
    multi project site. Going to push to test.

--- trac_deploy.sh
+++ trac_deploy.sh
@@ -11,6 +11,11 @@ PROJECT='MyProject'
 TRAC_ROOT='/var/trac'
 TRAC_INSTALL='Trac==1.0'
 
+TRAC_APACHE='$TRAC_ROOT/apache'
+TRAC_EGGS='$TRAC_ROOT/eggs'
+TRAC_SITES='$TRAC_ROOT/sites'
+TRAC_WSGI='$TRAC_APACHE/trac.wsgi'
+
 ##Install the packages you need
 apt-get install sqlite3 python-sqlite python-setuptools python-genshi python-pygments apache2 sendmail git subversion python-subversion libapache2-mod-wsgi libapache2-mod-python
 
@@ -18,13 +23,20 @@ apt-get install sqlite3 python-sqlite python-setuptools python-genshi python-pyg
 easy_install $TRAC_INSTALL
 
 ##mkdir /var/trac /var/trac/sites /var/trac/eggs /var/trac/apache
-mkdir -p $TRAC_ROOT/sites $TRAC_ROOT/eggs $TRAC_ROOT/apache
+mkdir -p $TRAC_SITES $TRAC_EGGS $TRAC_APACHE
 
 ##Creat your first project
-trac-admin $TRAC_ROOT/sites/$PROJECT initenv
+trac-admin $TRAC_SITES/$PROJECT initenv
 
 ##Copy the premade WSGI file to the trac root apache folder
-cp ./trac.wsgi -t $TRAC_ROOT/apache
+##There are two wsgi files that can be made, one for making trac use multipul 
+##projects, and one the uses only a single project. Configure accordenly.
+##NOTE: Only one (1) should be uncommented.
+
+##wsgi for single project (default)
+cp ./trac_single.wsgi $TRAC_WSGI
+##wsgi for multipul projects
+##cp ./trac_multi.wsgi $TRAC_WSGI
 
 ##Make a backup of the existing httpd.conf file and copy the new one to the 
 ##/etc/apache2 folder
@@ -44,7 +56,7 @@ read -s PASSWD
 htpasswd -bcm $TRAC_ROOT/.htpasswd $USER $PASSWD
 
 ##Give your new user privilages on Trac
-trac-admin $TRAC_ROOT/sites/$PROJECT permission add $USER TRAC_ADMIN
+trac-admin $TRAC_SITES/$PROJECT permission add $USER TRAC_ADMIN
 
 chown -R www-data $TRAC_ROOT
 service apache2 restart
