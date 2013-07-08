517371667e667d6754c6e3f0b661b1d8965ec04e
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2013-07-08 11:18:08 -0500

    Updated README.md and fixed several spelling mistakes.

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 About
 =====
 This is a little side project I'm working on to automate the deployment of a 
-Trac bug tracking host.
+Trac bug tracking host. This script is for deployting a single trac host, 
+however I have two `*.wsgi` files; `trac_single.wsgi` if for a single project on
+the Trac host, and `trac_multi.wsgi` if for having multiple projects on a 
+single Trac host. For my own needs, this will deploy a single project on the new
+Trac host.
 
-For the most part, it's done. I'm gong to look into also automating the
-instalation for some Trac plugins. Such as the web admin, and Git plugin.
+As a quick note, you may want to edit the line in `trac_deploy.sh` that has the 
+`PROJECT` variable to whatever you would like your project to be named. The 
+default value is "MyProject"
+
+
+What We Assume
+==============
+When this scrip was made, it is assumed that the server that is going to be used
+is a freshly installed server OS (Ubunut 12.04.2) and Apache has not be 
+configured yet.
+
+*NOTE: If you have already configured Apache comment out the line in the script 
+that moves and replaces `/etc/apache2/httpd.conf` you will need to then 
+configure that manually until I figure out a better why of adding that entry to 
+that config file*
+
+What it Does
+=============
+1. The script will install a series of programs and libs (sqlite, apache2, etc.)
+2. Create a series of directories in `/var/` where the Trac sites will be stored
+3. Initiate your first Trac environment
+4. Copy two configs to `/etc/apache2/` and `$TRAC_ROOT/apache/`
+5. Create your first Trac admin user w/ passwd
+6. Make `www-data` the owner of `$TRAC_ROOT`
+7. Restart Apache
 
 Resources
 =========
--- trac_deploy.sh
+++ trac_deploy.sh
@@ -6,9 +6,13 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+##Define the fist project's name
 PROJECT='MyProject'
+##Where to put the Trac environment
 TRAC_ROOT='/var/trac'
+##Which version of Trac to install; only one should be uncommented
 TRAC_INSTALL='Trac==1.0'
+#TRAC_INSATLL='Trac==dev'
 
 ##Install the packages you need
 apt-get install sqlite3 python-sqlite python-setuptools python-genshi python-pygments apache2 sendmail git subversion python-subversion libapache2-mod-wsgi libapache2-mod-python
@@ -16,20 +20,20 @@ apt-get install sqlite3 python-sqlite python-setuptools python-genshi python-pyg
 ##Install Trac
 easy_install $TRAC_INSTALL
 
-##mkdir /var/trac /var/trac/sites /var/trac/eggs /var/trac/apache
+##Make the containing folders for Trac
 mkdir -p $TRAC_ROOT/sites/ $TRAC_ROOT/eggs/ $TRAC_ROOT/apache/
 
-##Creat your first project
+##Create your first project
 trac-admin $TRAC_ROOT/sites/$PROJECT initenv
 
-##Copy the premade WSGI file to the trac root apache folder
-##There are two wsgi files that can be made, one for making trac use multipul 
-##projects, and one the uses only a single project. Configure accordenly.
+##Copy the pre-made WSGI file to the trac root Apache folder
+##There are two wsgi files that can be made, one for making trac use multiple 
+##projects, and one the uses only a single project. Configure accordantly.
 ##NOTE: Only one (1) should be uncommented.
 
 ##wsgi for single project (default)
 cp ./trac_single.wsgi $TRAC_ROOT/apache/trac.wsgi
-##wsgi for multipul projects
+##wsgi for multiple projects
 ##cp ./trac_multi.wsgi $TRAC_ROOT/apache/trac.wsgi
 
 ##Make a backup of the existing httpd.conf file and copy the new one to the 
@@ -37,21 +41,23 @@ cp ./trac_single.wsgi $TRAC_ROOT/apache/trac.wsgi
 mv /etc/apache2/httpd.conf /etc/apache2/httpd.conf.bak
 cp ./httpd.conf -t /etc/apache2/
 
-echo "\n"
-
-##User infor the TRAC_ADMIN
-echo "Creat your first user and admim"
+##User info for the TRAC_ADMIN
+echo "Create your first user and administrator"
 echo -n "New User: "
 read USER
 echo -n "Password: "
 read -s PASSWD
 
-##Creat the .htpasswd file for Trac
+##Create the .htpasswd file for Trac
 htpasswd -bcm $TRAC_ROOT/.htpasswd $USER $PASSWD
 
-##Give your new user privilages on Trac
+##Unset the PASSWD
+unset PASSWD
+
+##Give your new user privileges on Trac
 trac-admin $TRAC_ROOT/sites/$PROJECT permission add $USER TRAC_ADMIN
 
+##Own the $TRAC_ROOT
 chown -R www-data $TRAC_ROOT
+##Restart Apache
 service apache2 restart
-
